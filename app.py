f0ec4129e2399a7e907888321380dd61b88efb21
Stsam98 <132654431+Stsam98@users.noreply.github.com>
2025-07-29 23:20:13 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,190 +1,222 @@
-import os
-from flask import Flask, request, jsonify, abort
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flasgger import Swagger
-from dotenv import load_dotenv
+from flasgger import Swagger, swag_from
+import os
 
-load_dotenv()
-
+# --- Flask init ---
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///local.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-swagger = Swagger(app)
 db = SQLAlchemy(app)
+swagger = Swagger(app)
 
+# --- Модель ---
 class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    surname = db.Column(db.String(50))
-    position = db.Column(db.String(50))
-    city = db.Column(db.String(50))
+    name = db.Column(db.String(100), nullable=False)
+    surname = db.Column(db.String(100), nullable=False)
+    position = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
 
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "surname": self.surname,
-            "position": self.position,
-            "city": self.city
-        }
-
-@app.route('/employees', methods=['POST'])
-def create_employee():
-    """
-    Создание нового сотрудника
-    ---
-    tags:
-      - Employees
-    requestBody:
-      required: true
-      content:
-        application/json:
-          schema:
-            type: object
-            properties:
-              name:
-                type: string
-              surname:
-                type: string
-              position:
-                type: string
-              city:
-                type: string
-    responses:
-      201:
-        description: ID созданного сотрудника
-        content:
-          application/json:
-            schema:
-              type: object
-              properties:
-                id:
-                  type: integer
-    """
-    data = request.get_json()
-    if not data or not data.get('name'):
-        abort(400, 'Name is required')
-    emp = Employee(**data)
-    db.session.add(emp)
-    db.session.commit()
-    return jsonify({"id": emp.id}), 201
-
-@app.route('/employees/<int:employee_id>', methods=['DELETE'])
-def delete_employee(employee_id):
-    """
-    Удаление сотрудника по ID
-    ---
-    tags:
-      - Employees
-    parameters:
-      - name: employee_id
-        in: path
-        type: integer
-        required: true
-    responses:
-      204:
-        description: Успешно удалено
-    """
-    emp = Employee.query.get_or_404(employee_id)
-    db.session.delete(emp)
-    db.session.commit()
-    return '', 204
-
-@app.route('/employees', methods=['GET'])
-def get_all_employees():
-    """
-    Получение всех сотрудников
-    ---
-    tags:
-      - Employees
-    responses:
-      200:
-        description: Список сотрудников
-    """
-    employees = Employee.query.all()
-    return jsonify([e.to_dict() for e in employees])
-
-@app.route('/employees/<int:employee_id>', methods=['GET'])
-def get_employee_by_id(employee_id):
-    """
-    Получение сотрудника по ID
-    ---
-    tags:
-      - Employees
-    parameters:
-      - name: employee_id
-        in: path
-        type: integer
-        required: true
-    responses:
-      200:
-        description: Сотрудник найден
-    """
-    emp = Employee.query.get_or_404(employee_id)
-    return jsonify(emp.to_dict())
-
-@app.route('/employees/by-name/<string:name>', methods=['GET'])
-def get_employee_by_name(name):
-    """
-    Получение сотрудника по имени
-    ---
-    tags:
-      - Employees
-    parameters:
-      - name: name
-        in: path
-        type: string
-        required: true
-    responses:
-      200:
-        description: Сотрудник найден
-    """
-    emp = Employee.query.filter_by(name=name).first()
-    if not emp:
-        abort(404, 'Employee not found')
-    return jsonify(emp.to_dict())
-
-@app.route('/employees/<int:employee_id>', methods=['PATCH'])
-def update_employee(employee_id):
-    """
-    Обновление сотрудника
-    ---
-    tags:
-      - Employees
-    parameters:
-      - name: employee_id
-        in: path
-        type: integer
-        required: true
-    requestBody:
-      required: true
-      content:
-        application/json:
-          schema:
-            type: object
-            properties:
-              name:
-                type: string
-              surname:
-                type: string
-              position:
-                type: string
-              city:
-                type: string
-    responses:
-      200:
-        description: Сотрудник обновлён
-    """
-    emp = Employee.query.get_or_404(employee_id)
-    data = request.get_json()
-    for key in ['name', 'surname', 'position', 'city']:
-        if key in data:
-            setattr(emp, key, data[key])
-    db.session.commit()
-    return jsonify(emp.to_dict())
-# Важно: это выполнится всегда, и локально, и на Render
+# --- Создание таблиц ---
 with app.app_context():
     db.create_all()
+
+# --- Эндпоинты ---
+
+@app.route('/employee', methods=['POST'])
+@swag_from({
+    'tags': ['Employee'],
+    'parameters': [
+        {
+            'name': 'body',
+            'in': 'body',
+            'required': True,
+            'schema': {
+                'type': 'object',
+                'properties': {
+                    'name': {'type': 'string'},
+                    'surname': {'type': 'string'},
+                    'position': {'type': 'string'},
+                    'city': {'type': 'string'}
+                },
+                'required': ['name', 'surname', 'position', 'city']
+            }
+        }
+    ],
+    'responses': {
+        201: {
+            'description': 'Employee created',
+            'schema': {
+                'type': 'object',
+                'properties': {
+                    'id': {'type': 'integer'}
+                }
+            }
+        }
+    }
+})
+def create_employee():
+    data = request.get_json()
+    employee = Employee(**data)
+    db.session.add(employee)
+    db.session.commit()
+    return jsonify({'id': employee.id}), 201
+
+
+@app.route('/employee/<int:employee_id>', methods=['DELETE'])
+@swag_from({
+    'tags': ['Employee'],
+    'parameters': [
+        {'name': 'employee_id', 'in': 'path', 'type': 'integer', 'required': True}
+    ],
+    'responses': {
+        200: {'description': 'Deleted'}
+    }
+})
+def delete_employee(employee_id):
+    employee = Employee.query.get_or_404(employee_id)
+    db.session.delete(employee)
+    db.session.commit()
+    return jsonify({'message': 'Deleted'}), 200
+
+
+@app.route('/employee', methods=['GET'])
+@swag_from({
+    'tags': ['Employee'],
+    'responses': {
+        200: {
+            'description': 'List of all employees',
+            'schema': {
+                'type': 'array',
+                'items': {
+                    'type': 'object',
+                    'properties': {
+                        'id': {'type': 'integer'},
+                        'name': {'type': 'string'},
+                        'surname': {'type': 'string'},
+                        'position': {'type': 'string'},
+                        'city': {'type': 'string'}
+                    }
+                }
+            }
+        }
+    }
+})
+def get_all_employees():
+    employees = Employee.query.all()
+    return jsonify([{
+        'id': e.id,
+        'name': e.name,
+        'surname': e.surname,
+        'position': e.position,
+        'city': e.city
+    } for e in employees]), 200
+
+
+@app.route('/employee/<int:employee_id>', methods=['GET'])
+@swag_from({
+    'tags': ['Employee'],
+    'parameters': [
+        {'name': 'employee_id', 'in': 'path', 'type': 'integer', 'required': True}
+    ],
+    'responses': {
+        200: {
+            'description': 'Employee by ID',
+            'schema': {
+                'type': 'object',
+                'properties': {
+                    'id': {'type': 'integer'},
+                    'name': {'type': 'string'},
+                    'surname': {'type': 'string'},
+                    'position': {'type': 'string'},
+                    'city': {'type': 'string'}
+                }
+            }
+        }
+    }
+})
+def get_employee_by_id(employee_id):
+    e = Employee.query.get_or_404(employee_id)
+    return jsonify({
+        'id': e.id,
+        'name': e.name,
+        'surname': e.surname,
+        'position': e.position,
+        'city': e.city
+    })
+
+
+@app.route('/employee/by-name/<string:name>', methods=['GET'])
+@swag_from({
+    'tags': ['Employee'],
+    'parameters': [
+        {'name': 'name', 'in': 'path', 'type': 'string', 'required': True}
+    ],
+    'responses': {
+        200: {
+            'description': 'Employees by name',
+            'schema': {
+                'type': 'array',
+                'items': {
+                    'type': 'object',
+                    'properties': {
+                        'id': {'type': 'integer'},
+                        'name': {'type': 'string'},
+                        'surname': {'type': 'string'},
+                        'position': {'type': 'string'},
+                        'city': {'type': 'string'}
+                    }
+                }
+            }
+        }
+    }
+})
+def get_employee_by_name(name):
+    employees = Employee.query.filter_by(name=name).all()
+    return jsonify([{
+        'id': e.id,
+        'name': e.name,
+        'surname': e.surname,
+        'position': e.position,
+        'city': e.city
+    } for e in employees]), 200
+
+
+@app.route('/employee/<int:employee_id>', methods=['PUT'])
+@swag_from({
+    'tags': ['Employee'],
+    'parameters': [
+        {'name': 'employee_id', 'in': 'path', 'type': 'integer', 'required': True},
+        {
+            'name': 'body',
+            'in': 'body',
+            'required': True,
+            'schema': {
+                'type': 'object',
+                'properties': {
+                    'name': {'type': 'string'},
+                    'surname': {'type': 'string'},
+                    'position': {'type': 'string'},
+                    'city': {'type': 'string'}
+                }
+            }
+        }
+    ],
+    'responses': {
+        200: {'description': 'Employee updated'}
+    }
+})
+def update_employee(employee_id):
+    data = request.get_json()
+    employee = Employee.query.get_or_404(employee_id)
+    for field in ['name', 'surname', 'position', 'city']:
+        if field in data:
+            setattr(employee, field, data[field])
+    db.session.commit()
+    return jsonify({'message': 'Updated'}), 200
+
+
 if __name__ == '__main__':
     app.run(debug=True)
