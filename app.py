d0a17849b1631edd9296588696aef924625b6b3f
Stsam98 <132654431+Stsam98@users.noreply.github.com>
2025-07-31 19:10:03 +0400

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    username = db.Column(db.String(512), unique=True, nullable=False)
+    password_hash = db.Column(db.String(512), nullable=False)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
